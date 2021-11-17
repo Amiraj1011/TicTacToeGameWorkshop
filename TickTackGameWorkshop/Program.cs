fa7d5cf58489b08a49a4a07a50ae7931cd581423
“AMIR <amiraj1011@gmail.com>
2021-11-17 12:23:09 -0800

    amir for UC1 for display game board

---
+++ TickTackGameWorkshop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TickTackGameWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tick Tack Toe Game");
+           //creating object of TicTacToeGame class
+            TicTacToeGame game = new TicTacToeGame();
+            Console.WriteLine();
+            //calling display method
+            game.DisplayBoard();        }
+    }
+}
---
+++ TickTackGameWorkshop/TickTackGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTackGameWorkshop
+{
+    public class TicTacToeGame
+    {
+        //declaring a char data type array of size 10 to accomodate all the 9 places in the game these are set to be empty.
+        char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+        //displayiong the display board 
+        public void DisplayBoard()
+        {
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+
+            Console.WriteLine("     |     |      ");
+        }
+    }
+}
